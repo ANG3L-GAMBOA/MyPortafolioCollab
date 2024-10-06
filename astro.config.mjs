866949388cb1950b582d0674a7ce478110997cc6
ANG3L-GAMBOA <angelxdangerxdgamara@gmail.com>
2024-10-05 23:44:24 -0500

    dependences

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
-import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
+import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 
-import robotsTxt from "astro-robots-txt"
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
-})
+  output: 'server',
+  adapter: vercel(),
+});
