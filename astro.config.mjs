e7ee8c85b5febb5bca67c352dc542bf0ac5fcdf2
ANG3L-GAMBOA <angelxdangerxdgamara@gmail.com>
2024-10-06 00:19:25 -0500

    change output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
-import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import { defineConfig } from 'astro/config'
+import tailwind from "@astrojs/tailwind"
 
+import robotsTxt from "astro-robots-txt"
+
+// https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: vercel(),
-});
+  integrations: [tailwind(), robotsTxt()],
+    output: 'static'
+})
