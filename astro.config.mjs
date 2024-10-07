bf3106c19dc104f8d9d661b173be007369882dd5
ANG3L-GAMBOA <angelxdangerxdgamara@gmail.com>
2024-10-06 19:26:43 -0500

    eo

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import robotsTxt from "astro-robots-txt"
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-    output: 'static'
+  site: 'https://porfolio.dev/'
 })
